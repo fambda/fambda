967735e84eb62eb6478f8bd333595e95e1c9ab71
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-03-22 21:39:41 +0200

    Either struct (#21)

---
+++ Fambda.Tests/DataTypes/Log.cs
@@ -0,0 +1,22 @@
+namespace Fambda.Tests.DataTypes
+{
+    public static class Log
+    {
+        private static string _holder = null;
+
+        public static void Init()
+        {
+            _holder = null;
+        }
+
+        public static void Message(string message)
+        {
+            _holder = message;
+        }
+
+        public static string Read()
+        {
+            return _holder;
+        }
+    }
+}
--- Fambda.Tests/EitherTests.cs
+++ Fambda.Tests/EitherTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Fambda.Tests.DataTypes;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -96,6 +97,101 @@ namespace Fambda.Tests
             // Assert
             result.Should().Be(expectedResult);
         }
+
+        #endregion
+
+        #region Either
+
+        [TestMethod]
+        public void EitherMatchThroughFuncShouldMatchLeft()
+        {
+            // Arrange
+            var either = new Either<string, int>("left");
+
+            // Act
+            var result = either.Match(l => $"MatchLeft({l})", r => $"MatchRight({r})");
+
+            // Assert
+            result.Should().Be("MatchLeft(left)");
+        }
+
+        [TestMethod]
+        public void EitherMatchThroughFuncShouldMatchRight()
+        {
+            // Arrange
+            var either = new Either<string, int>(5);
+
+            // Act
+            var result = either.Match(l => $"MatchLeft({l})", r => $"MatchRight({r})");
+
+            // Assert
+            result.Should().Be("MatchRight(5)");
+        }
+
+        [TestMethod]
+        public void EitherMatchThroughActionShouldMatchLeft()
+        {
+            // Arrange
+            Log.Init();
+            var either = new Either<string, int>("left");
+
+            // Act
+            either.Match(
+                    Left: (l) => Log.Message($"MatchLeft({l})"),
+                    Right: (r) => Log.Message($"MatchRight{r}")
+                );
+            var result = Log.Read();
+
+            // Assert
+            result.Should().Be("MatchLeft(left)");
+        }
+
+        [TestMethod]
+        public void EitherMatchThroughActionShouldMatchRight()
+        {
+            // Arrange
+            Log.Init();
+            var either = new Either<string, int>(5);
+
+            // Act
+            either.Match(
+                    Left: (l) => Log.Message($"MatchLeft({l})"),
+                    Right: (r) => Log.Message($"MatchRight({r})")
+                );
+            var result = Log.Read();
+
+            // Assert
+            result.Should().Be("MatchRight(5)");
+        }
+
+        [TestMethod]
+        public void EitherToStringShouldReturnLeftRepresentation()
+        {
+            // Arrange
+            var either = new Either<string, int>("left");
+
+            // Act
+            var result = either.ToString();
+
+            // Assert
+            result.Should().Be("Left(left)");
+        }
+
+        [TestMethod]
+        public void EitherToStringShouldReturnRightRepresentation()
+        {
+            // Arrange
+            var either = new Either<string, int>(5);
+
+            // Act
+            var result = either.ToString();
+
+            // Assert
+            result.Should().Be("Right(5)");
+        }
+
+
+        
 
         #endregion
     }
