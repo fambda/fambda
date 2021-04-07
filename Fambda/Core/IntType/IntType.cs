dbe3573fc7af255ec7e1b71b0c5eae6971107dc3
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-04-07 20:06:14 +0300

    Basic corlib types Int, Long, Decimal, Bool (#47)

---
+++ Fambda/Core/IntType/IntType.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using static Fambda.F;
+
+namespace Fambda
+{
+    public static class IntType
+    {
+        /// <summary>
+        /// Converts the string representation of a number into <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert.</param>
+        /// <returns><see cref="Option{T}"/> with <see cref="OptionSome{T}"/> if <paramref name="s" /> was converted successfully; otherwise, <see cref="OptionNone"/></returns>
+        [Pure]
+        public static Option<int> Parse(string s)
+        {
+            if(int.TryParse(s, out var parseResult))
+            {
+                return Some(parseResult);
+            }
+            else
+            {
+                return None;
+            }
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number into <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert. The string is interpreted using the style specified by <paramref name="style" />.</param>
+        /// <param name="style">A bitwise combination of enumeration values that indicates the style elements that can be present in <paramref name="s" />. A typical value to specify is <see cref="System.Globalization.NumberStyles.Integer" />.</param>
+        /// <returns><see cref="Option{T}"/> with <see cref="OptionSome{T}"/> if <paramref name="s" /> was converted successfully; otherwise, <see cref="OptionNone"/></returns>
+        [Pure]
+        public static Option<int> Parse(string s, NumberStyles style)
+        {
+            try
+            {
+                var parseResult = int.Parse(s, style);
+                return Some(parseResult);
+            }
+            catch
+            {
+                return None;
+            }
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number into <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information about <paramref name="s" />.</param>
+        /// <returns><see cref="Option{T}"/> with <see cref="OptionSome{T}"/> if <paramref name="s" /> was converted successfully; otherwise, <see cref="OptionNone"/></returns>
+        [Pure]
+        public static Option<int> Parse(string s, IFormatProvider provider)
+        {
+            try {
+                var parseResult = int.Parse(s, provider);
+                return Some(parseResult);
+            }
+            catch
+            {
+                return None;
+            }
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number into <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="s">A string containing a number to convert. The string is interpreted using the style specified by <paramref name="style" />.</param>
+        /// <param name="style">A bitwise combination of enumeration values that indicates the style elements that can be present in <paramref name="s" />. A typical value to specify is <see cref="System.Globalization.NumberStyles.Integer" />.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information about <paramref name="s" />.</param>
+        /// <returns><see cref="Option{T}"/> with <see cref="OptionSome{T}"/> if <paramref name="s" /> was converted successfully; otherwise, <see cref="OptionNone"/></returns>
+        [Pure]
+        public static Option<int> Parse(string s, NumberStyles style, IFormatProvider provider)
+        {
+            if(int.TryParse(s, style, provider, out var parseResult))
+            { 
+                return Some(parseResult);
+            }
+            else
+            {
+                return None;
+            }
+        }
+    }
+}
