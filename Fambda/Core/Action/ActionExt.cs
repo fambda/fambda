36c021c5b8fcef890729ae738118c064a8b3c3e0
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-06-02 19:02:14 +0300

    More Action to Func conversions (#78)

--- Fambda/Core/Action/ActionExt.cs
+++ Fambda/Core/Action/ActionExt.cs
@@ -29,5 +29,19 @@ namespace Fambda
         /// <returns><see cref="Func{T1, T2, Unit}"/></returns>
         public static Func<T1, T2, Unit> ToFunc<T1, T2>(this Action<T1, T2> action)
             => (T1 t1, T2 t2) => { action(t1, t2); return Unit(); };
+
+        /// <summary>
+        /// Converts <see cref="Action{T1, T2, T3}"/> to <see cref="Func{T1, T2, T3, Unit}"/>.
+        /// </summary>
+        /// <returns><see cref="Func{T1, T2, T3, Unit}"/></returns>
+        public static Func<T1, T2, T3, Unit> ToFunc<T1, T2, T3>(this Action<T1, T2, T3> action)
+            => (T1 t1, T2 t2, T3 t3) => { action(t1, t2, t3); return Unit(); };
+
+        /// <summary>
+        /// Converts <see cref="Action{T1, T2, T3, T4}"/> to <see cref="Func{T1, T2, T3, T4, Unit}"/>.
+        /// </summary>
+        /// <returns><see cref="Func{T1, T2, T3, T4, Unit}"/></returns>
+        public static Func<T1, T2, T3, T4, Unit> ToFunc<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action)
+            => (T1 t1, T2 t2, T3 t3, T4 t4) => { action(t1, t2, t3, t4); return Unit(); };
     }
 }
