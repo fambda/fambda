7b622c35c760fee1392c18bfc51651f2e438a8a6
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-06-19 20:07:38 +0300

    Linq syntax (#82)

--- Fambda.Tests/Core/Option/OptionExtTests.cs
+++ Fambda.Tests/Core/Option/OptionExtTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -95,6 +95,104 @@ namespace Fambda.Tests
 
             // Assert
             result.Should().Be(None);
+        }
+
+        #endregion
+
+        #region Linq
+
+        [TestMethod]
+        public void LinqExpressionSingleFromClauseShouldSucceed()
+        {
+            // Arrange
+            var expectedResult = Some(1);
+
+            // Act
+            var result = from a in Some(1)
+                         select a;
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void LinqExpressionTwoFromClausesShouldSucceed()
+        {
+            // Arrange
+            var expectedResult = Some(3);
+
+            // Act
+            var result = from a in Some(1)
+                         from b in Some(2)
+                         select a + b;
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void LinqExpressionThreeFromClausesShouldSucceed()
+        {
+            // Arrange
+            var expectedResult = Some(6);
+
+            // Act
+            var result = from a in Some(1)
+                         from b in Some(2)
+                         from c in Some(3)
+                         select a + b + c;
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void LinqExpressionMultipleFromClausesShouldSucceed()
+        {
+            // Arrange
+            var expectedResult = Some(10);
+
+            // Act
+            var result = from a in Some(1)
+                         from b in Some(2)
+                         from c in Some(3)
+                         let sum = a + b + c
+                         from d in Some(sum)
+                         from e in Some(4)
+                         select d + e;
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void LinqExpressionFromWhereShouldReturnSome()
+        {
+            // Arrange
+            var expectedResult = Some(1);
+
+            // Act
+            var result = from a in Some(1)
+                         where a > 0
+                         select a;
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void LinqExpressionFromWhereShouldReturnNone()
+        {
+            // Arrange
+            var expectedResult = None;
+
+            // Act
+            var result = from a in Some(1)
+                         where a > 1
+                         select a;
+
+            // Assert
+            result.Should().Be(expectedResult);
         }
 
         #endregion
