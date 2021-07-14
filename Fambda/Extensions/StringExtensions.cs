ab93b0a839468a2795ac290e80ade3c5dbc13074
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-07-14 22:39:06 +0300

    Internal modifier (#98)

--- Fambda/Extensions/StringExtensions.cs
+++ Fambda/Extensions/StringExtensions.cs
@@ -1,11 +1,11 @@
-using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.Contracts;
 
 namespace Fambda.Extensions
 {
     /// <summary>
     /// String extensions.
     /// </summary>
-    public static class StringExtensions
+    internal static class StringExtensions
     {
         /// <summary>
         /// Indicates whether the specified not null string is empty.
@@ -13,7 +13,7 @@ namespace Fambda.Extensions
         /// <param name="str">The string to test.</param>
         /// <returns>true if the specified string is not null and empty; otherwise, false.</returns>
         [Pure]
-        public static bool IsEmpty(this string str)
+        internal static bool IsEmpty(this string str)
             => str != null && str.Length == 0;
 
         /// <summary>
@@ -22,7 +22,7 @@ namespace Fambda.Extensions
         /// <param name="str">The string to test.</param>
         /// <returns>true if the specified string is not null and consists exclusively of white-space characters; otherwise, false.</returns>
         [Pure]
-        public static bool IsWhiteSpace(this string str)
+        internal static bool IsWhiteSpace(this string str)
         {
             if (str != null)
             {
@@ -44,7 +44,7 @@ namespace Fambda.Extensions
         /// <param name="str">The string to test.</param>
         /// <returns>true if the specified string is not null, not empty and has leading spaces; otherwise, false.</returns>
         [Pure]
-        public static bool HasLeadingSpace(this string str)
+        internal static bool HasLeadingSpace(this string str)
             => str != null && str.Length > 0 && char.IsWhiteSpace(str, 0);
 
         /// <summary>
@@ -53,7 +53,7 @@ namespace Fambda.Extensions
         /// <param name="str">The string to test.</param>
         /// <returns>true if the specified string is not null, not empty and has trailing spaces; otherwise, false.</returns>
         [Pure]
-        public static bool HasTrailingSpace(this string str)
+        internal static bool HasTrailingSpace(this string str)
             => str != null && str.Length > 0 && char.IsWhiteSpace(str, str.Length - 1);
 
     }
