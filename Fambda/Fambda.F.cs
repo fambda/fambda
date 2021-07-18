d62cd660cbe8fb9e1ddc574cb0c0d751ef9a38ed
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-07-18 16:11:55 +0300

    Conversions into uncurried format (#102)

--- Fambda/Fambda.F.cs
+++ Fambda/Fambda.F.cs
@@ -62,5 +62,51 @@ namespace Fambda
             => (T1 t1) => (T2 t2) => (T3 t3) => (T4 t4) => func(t1, t2, t3, t4);
 
         #endregion
+
+        #region Uncurry
+
+        /// <summary>
+        /// Converts curried function to a function in uncurried format with two parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of first parameter.</typeparam>
+        /// <typeparam name="T2">The type of second parameter.</typeparam>
+        /// <typeparam name="Res">The type of the return value.</typeparam>
+        /// <param name="func">Function to be uncurried.</param>
+        /// <returns>Uncurried function.</returns>
+        /// <remarks>Transforms T1 -> T2 -> Res to (T1, T2) -> Res.</remarks>
+        [Pure]
+        public static Func<T1, T2, Res> Uncurry<T1, T2, Res>(Func<T1, Func<T2, Res>> func)
+            => (t1, t2) => func(t1)(t2);
+
+        /// <summary>
+        /// Converts curried function to a function in uncurried format with three parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of first parameter.</typeparam>
+        /// <typeparam name="T2">The type of second parameter.</typeparam>
+        /// <typeparam name="T3">The type of third parameter.</typeparam>
+        /// <typeparam name="Res">The type of the return value.</typeparam>
+        /// <param name="func">Function to be uncurried.</param>
+        /// <returns>Uncurried function.</returns>
+        /// <remarks>Transforms T1 -> T2 -> T3 -> Res to (T1, T2, T3) -> Res.</remarks>
+        [Pure]
+        public static Func<T1, T2, T3, Res> Uncurry<T1, T2, T3, Res>(Func<T1, Func<T2, Func<T3, Res>>> func)
+            => (t1, t2, t3) => func(t1)(t2)(t3);
+
+        /// <summary>
+        /// Converts curried function to a function in uncurried format with four parameters.
+        /// </summary>
+        /// <typeparam name="T1">The type of first parameter.</typeparam>
+        /// <typeparam name="T2">The type of second parameter.</typeparam>
+        /// <typeparam name="T3">The type of third parameter.</typeparam>
+        /// <typeparam name="T4">The type of fourth parameter.</typeparam>
+        /// <typeparam name="Res">The type of the return value.</typeparam>
+        /// <param name="func">Function to be uncurried.</param>
+        /// <returns>Uncurried function.</returns>
+        /// <remarks>Transforms T1 -> T2 -> T3 -> T4 -> Res to (T1, T2, T3, T4) -> Res.</remarks>
+        [Pure]
+        public static Func<T1, T2, T3, T4, Res> Uncurry<T1, T2, T3, T4, Res>(Func<T1, Func<T2, Func<T3, Func<T4, Res>>>> func)
+            => (t1, t2, t3, t4) => func(t1)(t2)(t3)(t4);
+
+        #endregion
     }
 }
