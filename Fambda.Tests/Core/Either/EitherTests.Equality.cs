8c0084e58213db4c8cb46ca453f050e401ed2668
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-09-27 13:30:46 +0300

    Align operator overloads (#127)

---
+++ Fambda.Tests/Core/Either/EitherTests.Equality.cs
@@ -0,0 +1,12 @@
+using Fambda.Tests.DataTypes;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fambda.Tests
+{
+
+    public partial class EitherTests
+    {
+
+    }
+}
