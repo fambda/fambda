696d0cce4d67ed5acb082e8118ce0ec5ecdb8b16
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-09-27 21:01:04 +0300

    Rearrange record (#129)

--- Fambda/Core/Record/Record.cs
+++ Fambda/Core/Record/Record.cs
@@ -20,48 +20,10 @@ namespace Fambda
         /// </summary>
         protected abstract IEnumerable<object> GetEqualityComponents();
 
-
-        #region Overload operators
-
-        /// <summary>
-        /// Indicates whether the left <see cref="Record{T}"/> is equal to the right <see cref="Record{T}"/>
-        /// </summary>
-        /// <param name="left"><see cref="Record{T}"/> object.</param>
-        /// <param name="right"><see cref="Record{T}"/> object.</param>
-        /// <returns>true if left <see cref="Record{T}"/> object is equal to the right parameter; otherwise, false.</returns>
-        public static bool operator ==(Record<T> left, Record<T> right)
-        {
-            if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
-            {
-                return true;
-            }
-
-            if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
-            {
-                return false;
-            }
-
-            var result = left.Equals(right);
-
-            return result;
-        }
-
-        /// <summary>
-        /// Indicates whether the left <see cref="Record{T}"/> is not equal to the right <see cref="Record{T}"/>
-        /// </summary>
-        /// <param name="left"><see cref="Record{T}"/> object.</param>
-        /// <param name="right"><see cref="Record{T}"/> object.</param>
-        /// <returns>true if left <see cref="Record{T}"/> object is not equal to the right parameter; otherwise, false.</returns>
-        public static bool operator !=(Record<T> left, Record<T> right)
-            => !(left == right);
-
-
-        #endregion
-
         #region Structural hashing
 
         /// <summary>
-        /// Calculates the hash-code based on equality components.
+        /// Returns the hash-code based on equality components.
         /// </summary>
         /// <returns>A hash code for the current <see cref="Record{T}"/> object.</returns>
         public override int GetHashCode()
@@ -75,6 +37,14 @@ namespace Fambda
         #endregion
 
         #region Structural equality
+
+        /// <summary>
+        /// Determines whether specified object is equal to the current <see cref="Record{T}"/> object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current <see cref="Record{T}"/> object.</param>
+        /// <returns>true if the specified object is equal to the current <see cref="Record{T}"/> object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+            => Equals(obj as T);
 
         /// <summary>
         /// Indicates whether the current <see cref="Record{T}"/> is equal to another <see cref="Record{T}"/>
@@ -91,17 +61,47 @@ namespace Fambda
             return EqualsComponents(other);
         }
 
-        /// <summary>
-        /// Determines whether specified object is equal to the current <see cref="Record{T}"/> object.
-        /// </summary>
-        /// <param name="obj">The object to compare with the current <see cref="Record{T}"/> object.</param>
-        /// <returns>true if the specified object is equal to the current <see cref="Record{T}"/> object; otherwise, false.</returns>
-        public override bool Equals(object obj)
-            => Equals(obj as T);
-
-
         private bool EqualsComponents(Record<T> other)
             => GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+
+
+        #endregion
+
+        #region Overload operators
+
+        /// <summary>
+        /// Compares two <see cref="Record{T}"/> objects through equality operator.
+        /// </summary>
+        /// <param name="lhs"><see cref="Record{T}"/> left hand side object.</param>
+        /// <param name="rhs"><see cref="Record{T}"/> right hand side object.</param>
+        /// <returns>true if lhs is equal to the rhs; otherwise, false.</returns>
+        public static bool operator ==(Record<T> lhs, Record<T> rhs)
+        {
+            if (ReferenceEquals(lhs, null) && ReferenceEquals(rhs, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(lhs, null) || ReferenceEquals(rhs, null))
+            {
+                return false;
+            }
+
+            var result = lhs.Equals(rhs);
+
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// Compares two <see cref="Record{T}"/> objects through inequality operator.
+        /// </summary>
+        /// <param name="lhs"><see cref="Record{T}"/> left hand side object.</param>
+        /// <param name="rhs"><see cref="Record{T}"/> right hand side object.</param>
+        /// <returns>true if the lhs object is not equal to rhs; otherwise, false.</returns>
+        public static bool operator !=(Record<T> lhs, Record<T> rhs)
+            => !(lhs == rhs);
 
 
         #endregion
