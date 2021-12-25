c2336d8cbd05ee63c19ecb633f0887d6f8b87a46
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2021-12-25 21:55:58 +0200

    Hashable and eq either type classes  (#151)

---
+++ Fambda/TypeClasses/Instances/HashableEither.cs
@@ -0,0 +1,17 @@
+
+namespace Fambda
+{
+    /// <summary>
+    /// Either type hash-code.
+    /// </summary>
+    public struct HashableEither<L, R> : Hashable<Either<L, R>>
+    {
+        /// <summary>
+        /// Get hash code of the value.
+        /// </summary>
+        /// <param name="either"><see cref="Either{L,R}"/> to get the hash-code.</param>
+        /// <returns>The hash-code of <see cref="Either{L,R}"/>.</returns>
+        public int GetHashCode(Either<L, R> either)
+            => either.ToString().GetHashCode();
+    }
+}
