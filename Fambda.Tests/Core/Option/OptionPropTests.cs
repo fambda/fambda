9da9fd84d50fb8b2f2b6c8cf8448b5af3a27f56b
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-01-18 23:05:09 +0200

    Prop option tests (#166)

---
+++ Fambda.Tests/Core/Option/OptionPropTests.cs
@@ -0,0 +1,49 @@
+using System;
+using FsCheck;
+using Xunit;
+
+namespace Fambda.Tests
+{
+    public class OptionPropTests
+    {
+        public OptionPropTests()
+        {
+            Arb.Register<OptionArbitraries>();
+        }
+
+        [Fact]
+        public void ToStringShouldReturnExpectedRepresentation()
+        {
+            Prop.ForAll<Option<int>>(option => (option.ToString().StartsWith("Some(") && option.ToString().EndsWith(")")) || option.ToString() == "None")
+                .VerboseCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void MapShouldBehaveAsLinqExpression()
+        {
+            Func<string, string> append = s => s + "_";
+
+            Prop.ForAll<Option<string>>(option => option.Map(append) == (from x in option select append(x)))
+                .VerboseCheckThrowOnFailure();
+        }
+
+        internal class OptionArbitraries
+        {
+            public static Arbitrary<Option<T>> Option<T>()
+            {
+                return Gen.Sized(OptionGenerator.Generator<T>).ToArbitrary();
+            }
+        }
+
+        internal static class OptionGenerator
+        {
+            public static Gen<Option<T>> Generator<T>(int depth)
+            {
+                return Gen.OneOf(new Gen<Option<T>>[] {
+                    from value in Arb.Generate<T>()
+                    select value != null ? F.Some(value) : F.None
+                });
+            }
+        }
+    }
+}
