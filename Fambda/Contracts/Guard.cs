f564f8da598c22636bac86f020347ce35af8c6ea
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-03-14 14:56:00 +0200

    Updated cref attributes (#188)

--- Fambda/Contracts/Guard.cs
+++ Fambda/Contracts/Guard.cs
@@ -8,10 +8,10 @@ namespace Fambda.Contracts
         /// <summary>
         /// Guard on <typeparamref name="T" />.
         /// </summary>
-        /// <typeparam name="T">Bound value type</typeparam>
-        /// <param name="value">T value</param>
-        /// <param name="guardException"><see cref="GuardException"/></param>
-        /// <returns><see cref="Guard{T}"/>.</returns>
+        /// <typeparam name="T">The type of bound value.</typeparam>
+        /// <param name="value">The <typeparamref name="T"/> value.</param>
+        /// <param name="guardException"><see cref="GuardException"/>.</param>
+        /// <returns><see cref="Guard{T}">Guard&lt;T></see>.</returns>
         public static Guard<T> On<T>(T value, GuardException guardException)
         {
             return new Guard<T>(value, guardException);
@@ -20,9 +20,9 @@ namespace Fambda.Contracts
         /// <summary>
         /// Guard on <see cref="T:object[]"/>.
         /// </summary>
-        /// <param name="values">object values</param>
-        /// <param name="guardException"><see cref="GuardException"/></param>
-        /// <returns><see cref="Guard{object[]}"/>.</returns>
+        /// <param name="values">The object values.</param>
+        /// <param name="guardException"><see cref="GuardException"/>.</param>
+        /// <returns><see cref="Guard{T}">Guard&lt;object[]></see>.</returns>
         public static Guard<object[]> On(object[] values, GuardException guardException)
         {
             return new Guard<object[]>(values, guardException);
