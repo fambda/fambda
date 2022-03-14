e9efa6a27ad4833ba6103a70102c675c08169c2a
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-03-14 11:52:41 +0200

    Exceptional type applicative (#187)

--- Fambda/Core/Exceptional/ExceptionalExt.cs
+++ Fambda/Core/Exceptional/ExceptionalExt.cs
@@ -7,17 +7,60 @@ namespace Fambda
     /// </summary>
     public static class ExceptionalExt
     {
+        #region Map
+
         /// <summary>
         /// Maps <see cref="Exceptional{T}"/> into <see cref="Exceptional{Res}"/>
         /// </summary>
         public static Exceptional<Res> Map<R, Res>(this Exceptional<R> exceptional, Func<R, Res> func)
             => exceptional.Exception == null ? func(exceptional.Value) : new Exceptional<Res>(exceptional.Exception);
 
+        #endregion
+
+        #region Bind
+
         /// <summary>
         /// Binds <see cref="Exceptional{T}"/> into <see cref="Exceptional{Res}"/>
         /// </summary>
         public static Exceptional<Res> Bind<R, Res>(this Exceptional<R> exceptional, Func<R, Exceptional<Res>> func)
             => exceptional.Exception == null ? func(exceptional.Value) : new Exceptional<Res>(exceptional.Exception);
+
+        #endregion
+
+        #region Apply
+
+        /// <summary>
+        /// <para>Apply function <see cref="Func{T, Res}">Func&lt;T, Res&gt;</see> to unwrapped value of type <c>T</c>, and wrap the result back in an <c>Exceptional</c> (<see cref="Exceptional{T}">Exceptional&lt;Res&gt;</see>).</para>
+        /// <para><c>Exceptional&lt;T → Res> → Exceptional&lt;T> → Exceptional&lt;Res></c></para>
+        /// </summary>
+        /// <typeparam name="T">The type of wrapped function's parameter.</typeparam>
+        /// <typeparam name="Res">The type of the wrapped function's return value.</typeparam>
+        /// <param name="exceptionalF">The exceptional containing the function <see cref="Func{T, Res}"/> to be applied on the applicative.</param>
+        /// <param name="exceptionalT">The exceptional containing the value of type <c>T</c>, on which the function <see cref="Func{T, Res}"/> will be applied.</param>
+        /// <returns>The result of type <c>Res</c> wrapped in an <c>Exceptional</c> applicative (<see cref="Exceptional{T}">Exceptional&lt;Res&gt;</see>).</returns>
+        public static Exceptional<Res> Apply<T, Res>(this Exceptional<Func<T, Res>> exceptionalF, Exceptional<T> exceptionalT)
+           => exceptionalF.Match(
+                              Exception: ex => ex,
+                              Success: func => exceptionalT.Match(
+                                                              Exception: ex => ex,
+                                                              Success: t => new Exceptional<Res>(func(t))
+                                                            )
+                           );
+
+        /// <summary>
+        /// <para>Apply function <see cref="Func{T1, T2, Res}">Func&lt;T1, T2, Res&gt;</see> to <see cref="Exceptional{T}">Exceptional&lt;T1&gt;</see>'s unwrapped value of type <c>T1</c>, and wrap the result <see cref="Func{T2, Res}">Func&lt;T2, Res&gt;</see> back in an <c>Exceptional</c> (<see cref="Exceptional{T}">Exceptional&lt;Func&lt;T2, Res&gt;&gt;</see>).</para>
+        /// <para><c>Exceptional&lt;T1 → T2 → Res> → Exceptional&lt;T1> → Exceptional&lt;T2 → Res></c></para>
+        /// </summary>
+        /// <typeparam name="T1">The type of wrapped function's first parameter.</typeparam>
+        /// <typeparam name="T2">The type of wrapped function's second parameter.</typeparam>
+        /// <typeparam name="Res">The type of the wrapped function's return value.</typeparam>
+        /// <param name="exceptionalF">The exceptional containing the function <see cref="Func{T1, T2, Res}"/> to be applied on the applicative.</param>
+        /// <param name="exceptionalT1">The exceptional containing the value of type <c>T1</c>, on which the function <see cref="Func{T1, T2, Res}"/> will be applied.</param>
+        /// <returns>An unary function <see cref="Func{T2, Res}">Func&lt;T2, Res&gt;</see> wrapped in an <c>Exceptional</c> applicative (<see cref="Exceptional{T}">Exceptional&lt;Func&lt;T2, Res&gt;&gt;</see>).</returns>
+        public static Exceptional<Func<T2, Res>> Apply<T1, T2, Res>(this Exceptional<Func<T1, T2, Res>> exceptionalF, Exceptional<T1> exceptionalT1)
+           => Apply(exceptionalF.Map(F.Curry), exceptionalT1);
+
+        #endregion
 
     }
 }
