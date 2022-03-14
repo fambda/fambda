4c6ab25147e47c152132f618a00d720d888a3a5a
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-03-14 09:29:45 +0200

    Applicative (#183)

--- Fambda/Core/Option/OptionExt.cs
+++ Fambda/Core/Option/OptionExt.cs
@@ -28,6 +28,8 @@ namespace Fambda
 
         #endregion
 
+        #region Bind
+
         /// <summary>
         /// Binds <see cref="Option{T}"/> into <see cref="Option{Res}"/>
         /// </summary>
@@ -36,6 +38,43 @@ namespace Fambda
                         None: () => None,
                         Some: (t) => func(t)
                       );
+
+        #endregion
+
+        #region Apply
+
+        /// <summary>
+        /// <para>Apply function <see cref="Func{T, Res}">Func&lt;T, Res&gt;</see> to unwrapped value of type <c>T</c>, and wrap the result back in an <c>Option</c> (<see cref="Option{T}">Option&lt;Res&gt;</see>).</para>
+        /// <para><c>Option&lt;T → Res> → Option&lt;T> → Option&lt;Res></c></para>
+        /// </summary>
+        /// <typeparam name="T">The type of wrapped function's parameter.</typeparam>
+        /// <typeparam name="Res">The type of the wrapped function's return value.</typeparam>
+        /// <param name="optionF">The option containing the function <see cref="Func{T, Res}"/> to be applied on the applicative.</param>
+        /// <param name="optionT">The option containing the value of type <c>T</c>, on which the function <see cref="Func{T, Res}"/> will be applied.</param>
+        /// <returns>The result of type <c>Res</c> wrapped in an <c>Option</c> applicative (<see cref="Option{T}">Option&lt;Res&gt;</see>).</returns>
+        public static Option<Res> Apply<T, Res>(this Option<Func<T, Res>> optionF, Option<T> optionT)
+           => optionF.Match(
+                          None: () => None,
+                          Some: func => optionT.Match(
+                                                  None: () => None,
+                                                  Some: t => Some(func(t))
+                                               )
+                      );
+
+        /// <summary>
+        /// <para>Apply function <see cref="Func{T1, T2, Res}">Func&lt;T1, T2, Res&gt;</see> to <see cref="Option{T}">Option&lt;T1&gt;</see>'s unwrapped value of type <c>T1</c>, and wrap the result <see cref="Func{T2, Res}">Func&lt;T2, Res&gt;</see> back in an <c>Option</c> (<see cref="Option{T}">Option&lt;Func&lt;T2, Res&gt;&gt;</see>).</para>
+        /// <para><c>Option&lt;T1 → T2 → Res> → Option&lt;T1> → Option&lt;T2 → Res></c></para>
+        /// </summary>
+        /// <typeparam name="T1">The type of wrapped function's first parameter.</typeparam>
+        /// <typeparam name="T2">The type of wrapped function's second parameter.</typeparam>
+        /// <typeparam name="Res">The type of the wrapped function's return value.</typeparam>
+        /// <param name="optionF">The option containing the function <see cref="Func{T1, T2, Res}"/> to be applied on the applicative.</param>
+        /// <param name="optionT1">The option containing the value of type <c>T1</c>, on which the function <see cref="Func{T1, T2, Res}"/> will be applied.</param>
+        /// <returns>An unary function <see cref="Func{T2, Res}">Func&lt;T2, Res&gt;</see> wrapped in an <c>Option</c> applicative (<see cref="Option{T}">Option&lt;Func&lt;T2, Res&gt;&gt;</see>).</returns>
+        public static Option<Func<T2, Res>> Apply<T1, T2, Res>(this Option<Func<T1, T2, Res>> optionF, Option<T1> optionT1)
+           => Apply(optionF.Map(F.Curry), optionT1);
+
+        #endregion
 
         #region Linq
 
