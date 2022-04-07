a8b27d330764bccdd7f035d1b73ea8c55e7350ef
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-07 20:56:06 +0300

    Flatten nested enumerable (#210)

--- Fambda/Core/Enumerable/EnumerableExt.cs
+++ Fambda/Core/Enumerable/EnumerableExt.cs
@@ -20,5 +20,12 @@ namespace Fambda
         /// </summary>
         public static IEnumerable<Res> Bind<T, Res>(this IEnumerable<T> enumerable, Func<T, IEnumerable<Res>> func)
             => enumerable.SelectMany(func);
+
+        /// <summary>
+        /// <para>Flattens <see cref="IEnumerable{T}">IEnumerable&lt;IEnumerable&lt;T>></see> into <see cref="IEnumerable{T}">IEnumerable&lt;T></see>.</para>
+        /// <para><c>IEnumerable&lt;IEnumerable&lt;T>> → IEnumerable&lt;T></c></para>
+        /// </summary>
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> enumerable)
+            => enumerable.SelectMany(x => x);
     }
 }
