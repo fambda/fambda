d965d870a307e4bebe806618055ea14bce53df21
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-23 17:54:03 +0300

    Option boxed equals tests (#228)
    remark:
     between different states of Option

--- Fambda.Tests/Core/Option/OptionTests.Equality.cs
+++ Fambda.Tests/Core/Option/OptionTests.Equality.cs
@@ -65,6 +65,66 @@ namespace Fambda
         }
 
         [Fact]
+        public void Equals_BoxedOptionInNoneState_ReturnsTrue()
+        {
+            // Arrange
+            Option<int> optionA = new OptionNone();
+            Option<int> optionB = new OptionNone();
+            var boxedOptionB = optionB as object;
+
+            // Act
+            var result = optionA.Equals(boxedOptionB);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_BoxedOptionInNoneState_ReturnsFalse()
+        {
+            // Arrange
+            Option<int> optionA = new OptionSome<int>(1);
+            Option<int> optionB = new OptionNone();
+            var boxedOptionB = optionB as object;
+
+            // Act
+            var result = optionA.Equals(boxedOptionB);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_BoxedOptionInSomeState_ReturnsTrue()
+        {
+            // Arrange
+            Option<int> optionA = new OptionSome<int>(1);
+            Option<int> optionB = new OptionSome<int>(1);
+            var boxedOptionB = optionB as object;
+
+            // Act
+            var result = optionA.Equals(boxedOptionB);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_BoxedOptionInSomeState_ReturnsFalse()
+        {
+            // Arrange
+            Option<int> optionA = new OptionSome<int>(1);
+            Option<int> optionB = new OptionSome<int>(2);
+            var boxedOptionB = optionB as object;
+
+            // Act
+            var result = optionA.Equals(boxedOptionB);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
         public void Equals_BoxedOptionNone_ReturnsFalse()
         {
             // Arrange
