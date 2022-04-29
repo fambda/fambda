f685fd566b7d0df7be03ca369b5e680532ada5a1
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-29 15:18:05 +0300

    Try functor laws (#256)

---
+++ Fambda.Tests/Core/Try/TryTests.Laws.cs
@@ -0,0 +1,48 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Fambda
+{
+    public partial class TryTests
+    {
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Identity_Holds()
+        {
+            // Arrange
+            var value = "value";
+            Exceptional<string> exceptional() => value;
+            Try<string> @try = exceptional;
+
+            // Act
+            var mapped = @try.Map(F.Identity)();
+            var original = @try();
+            var result = mapped.Equals(original);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Composition_Holds()
+        {
+            // Arrange
+            var value = 1;
+            Exceptional<int> exceptional() => value;
+            Try<int> @try = exceptional;
+            Func<int, int> f = x => x + 1;
+            Func<int, int> g = x => x * 2;
+            var h = g.Compose(f);
+
+            // Act
+            var mapMap = @try.Map(f).Map(g)();
+            var mapCompose = @try.Map(h)();
+            var result = mapMap.Equals(mapCompose);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
