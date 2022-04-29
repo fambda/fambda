635d8ab65a234718615b9678794f6a1a91de533a
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-29 14:13:50 +0300

    Verify exceptional meet functor laws (#254)
    notes:
     Exceptional type also obeys first and second functor laws.

---
+++ Fambda.Tests/Core/Exceptional/ExceptionalTests.Laws.cs
@@ -0,0 +1,46 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Fambda
+{
+    public partial class ExceptionalTests
+    {
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Identity_Holds()
+        {
+            // Arrange
+            var value = "value";
+            Exceptional<string> exceptional = value;
+
+            // Act
+            var mapped = exceptional.Map(F.Identity);
+            var original = exceptional;
+            var result = mapped.Equals(original);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Composition_Holds()
+        {
+            // Arrange
+            var value = 1;
+            Exceptional<int> exceptional = value;
+            Func<int, int> f = x => x + 1;
+            Func<int, int> g = x => x * 2;
+            var h = g.Compose(f);
+
+            // Act
+            var mapMap = exceptional.Map(f).Map(g);
+            var mapCompose = exceptional.Map(h);
+            var result = mapMap.Equals(mapCompose);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
