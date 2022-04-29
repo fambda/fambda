408831bd522734c7023df1abe892eed321df5df2
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-29 18:30:38 +0300

    Definition convention with self (#260)
    notes:
     - simplification through this convention applied
       on definitions of data types` extensions
       Enumerable, Option, Either, Try, Exceptional

--- Fambda/Core/Enumerable/EnumerableExt.cs
+++ Fambda/Core/Enumerable/EnumerableExt.cs
@@ -12,20 +12,20 @@ namespace Fambda
         /// <summary>
         /// Maps <see cref="IEnumerable{T}"/> into <see cref="IEnumerable{Res}"/>
         /// </summary>
-        public static IEnumerable<Res> Map<T, Res>(this IEnumerable<T> enumerable, Func<T, Res> func)
-            => enumerable.Select(func);
+        public static IEnumerable<Res> Map<T, Res>(this IEnumerable<T> self, Func<T, Res> func)
+            => self.Select(func);
 
         /// <summary>
         /// Binds <see cref="IEnumerable{T}"/> into <see cref="IEnumerable{Res}"/>
         /// </summary>
-        public static IEnumerable<Res> Bind<T, Res>(this IEnumerable<T> enumerable, Func<T, IEnumerable<Res>> func)
-            => enumerable.SelectMany(func);
+        public static IEnumerable<Res> Bind<T, Res>(this IEnumerable<T> self, Func<T, IEnumerable<Res>> func)
+            => self.SelectMany(func);
 
         /// <summary>
         /// <para>Flattens <see cref="IEnumerable{T}">IEnumerable&lt;IEnumerable&lt;T>></see> into <see cref="IEnumerable{T}">IEnumerable&lt;T></see>.</para>
         /// <para><c>IEnumerable&lt;IEnumerable&lt;T>> → IEnumerable&lt;T></c></para>
         /// </summary>
-        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> enumerable)
-            => enumerable.SelectMany(x => x);
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> self)
+            => self.SelectMany(x => x);
     }
 }
