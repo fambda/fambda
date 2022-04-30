6a4c27a7ef86af7826528cec09f99bce8059c6ec
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-04-30 21:59:54 +0300

    Functor laws (#272)

---
+++ Fambda.Tests/Core/EnumerableTests.Laws.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Fambda
+{
+    public partial class EnumerableTests
+    {
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Identity_Holds()
+        {
+            // Arrange
+            var value = "value";
+            IEnumerable<string> enumerable = new List<string> { value };
+
+            // Act
+            var mapped = enumerable.Map(F.Identity);
+            var original = enumerable;
+
+            // Assert
+            mapped.Should().BeEquivalentTo(original);
+        }
+
+        [Fact]
+        [Trait("Category", "Laws")]
+        public void LawFunctor_Composition_Holds()
+        {
+            // Arrange
+            var value = 1;
+            IEnumerable<int> enumerable = new List<int> { value };
+            Func<int, int> f = x => x + 1;
+            Func<int, int> g = x => x * 2;
+            var h = g.Compose(f);
+
+            // Act
+            var mapMap = enumerable.Map(f).Map(g);
+            var mapCompose = enumerable.Map(h);
+
+            // Assert
+            mapMap.Should().BeEquivalentTo(mapCompose);
+        }
+    }
+}
