c9a7dec4f6d065870ea84ffaf9daaccb6cf61fc9
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-08-28 17:01:37 +0300

    Some construction with nullable values (#344)
    notes:
     - Some construction succeeds with not null nullable value
       and throws ArgumentNullException when null provided.

--- Fambda/Core/Option/OptionSome.cs
+++ Fambda/Core/Option/OptionSome.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 
 namespace Fambda
@@ -11,9 +12,9 @@ namespace Fambda
     {
         internal T Value { get; }
 
-        internal OptionSome(T value)
+        internal OptionSome([NotNull] T? value)
         {
-            Value = value;
+            Value = value ?? throw new ArgumentNullException(nameof(value));
         }
 
         /// <summary>
