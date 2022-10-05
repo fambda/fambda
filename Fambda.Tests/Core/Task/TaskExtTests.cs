04214871e21ee45c2b6763e99b4990209295e7ff
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-10-05 21:55:07 +0300

    Task linq declarative query syntax (#354)

--- Fambda.Tests/Core/Task/TaskExtTests.cs
+++ Fambda.Tests/Core/Task/TaskExtTests.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Fambda.DataTypes;
 using FluentAssertions;
 using Xunit;
+
+using static Fambda.F;
 
 namespace Fambda
 {
@@ -21,6 +26,82 @@ namespace Fambda
 
             // Assert
             result.Should().Be(expected);
+        }
+
+        #endregion
+
+        #region Linq
+
+        [Fact]
+        public async void Linq_SingleFromClause_Succeeds()
+        {
+            // Arrange
+            var expectedResult = Some(1);
+
+            // Act
+            var result = await (
+                            from x in TaskSucc(Some(1))
+                            select x
+                         );
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public async void Linq_TwoFromClauses_Succeeds()
+        {
+            // Arrange
+            var expectedResult = Some(3);
+
+            // Act
+            var result = await (
+                             from a in TaskSucc(Some(1))
+                             from b in TaskSucc(Some(2))
+                             select a.Map(x => x + b.Match(
+                                                        None: () => 0,
+                                                        Some: (i) => i
+                                                    )
+                                      )
+                         );
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public async void Linq_ThreeFromClauses_Succeeds()
+        {
+            // Arrange
+            var expectedResult = Some(6);
+
+            // Act
+            var result = await (
+                             from a in TaskSucc(Some(1))
+                             from b in TaskSucc(Some(2))
+                             from c in TaskSucc(Some(3))
+                             select Some(a.Match(None: () => 0, Some: (i) => i) +
+                                         b.Match(None: () => 0, Some: (i) => i) +
+                                         c.Match(None: () => 0, Some: (i) => i))
+                         );
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void Linq_Exceptions_Succeeds()
+        {
+            // Arrange
+            var task = from x in Task.Factory.StartNew<int>(() => { Thread.Sleep(100); throw new SomeException(); })
+                       from y in Task.Factory.StartNew<int>(() => { throw new AnotherException(); })
+                       select x + y;
+
+            // Act
+            Func<Task> act = async () => { await task; };
+
+            // Assert
+            act.Should().ThrowAsync<SomeException>();
         }
 
         #endregion
--- Fambda/Core/Task/TaskExt.cs
+++ Fambda/Core/Task/TaskExt.cs
@@ -18,5 +18,31 @@ namespace Fambda
             => func(await self.ConfigureAwait(false));
 
         #endregion
+
+        #region Linq
+
+        /// <summary>
+        /// Linq Select for <see cref="Task{T}">Task&lt;T></see>.
+        /// </summary>
+        public static async Task<Res> Select<T, Res>(this Task<T> self, Func<T, Res> func)
+            => func(await self.ConfigureAwait(false));
+
+        /// <summary>
+        /// Linq SelectMany for <see cref="Task{T}">Task&lt;T></see>.
+        /// </summary>
+        public static async Task<Res> SelectMany<T, Res>(this Task<T> self, Func<T, Task<Res>> func)
+            => await func(await self.ConfigureAwait(false)).ConfigureAwait(false);
+
+        /// <summary>
+        /// Linq SelectMany for <see cref="Task{T}">Task&lt;T></see>.
+        /// </summary>
+        public static async Task<Res> SelectMany<T, R, Res>(this Task<T> self, Func<T, Task<R>> func, Func<T, R, Res> project)
+        {
+            var t = await self.ConfigureAwait(false);
+            var r = await func(t).ConfigureAwait(false);
+            return project(t, r);
+        }
+
+        #endregion
     }
 }
