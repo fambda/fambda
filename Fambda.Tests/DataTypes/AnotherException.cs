23bc80eaffae1fd6e160640ee8213ef2b8752f5f
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-10-05 21:55:07 +0300

    Task linq declarative query syntax (#354)

---
+++ Fambda.Tests/DataTypes/AnotherException.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Fambda.DataTypes
+{
+    [Serializable]
+    public class AnotherException : Exception
+    {
+        public AnotherException() : base() { }
+
+        public AnotherException(string message) : base(message) { }
+
+        protected AnotherException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
