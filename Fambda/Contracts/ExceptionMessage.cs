4557014102e9e4540c5255b8e27eff6a2ff514e8
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2022-11-26 14:13:06 +0200

    Guards are no longer in use (#366)

--- Fambda/Contracts/ExceptionMessage.cs
+++
@@ -1,8 +0,0 @@
-namespace Fambda.Contracts
-{
-    internal static class ExceptionMessage
-    {
-        internal const string EachParamMustNotBeNull = "Each param must not be null.";
-        internal const string OptionValueMustNotBeNull = "Option value must not be null.";
-    }
-}
