44dc0efb745b2f0a7c5155673faf2ae28f6fded4
Pattern Driven <33223519+patterndriven@users.noreply.github.com>
2026-02-26 18:25:34 +0200

    Target framework and workflows dotnet 8 (#388)

--- Fambda.Tests/DataTypes/AnotherException.cs
+++ Fambda.Tests/DataTypes/AnotherException.cs
@@ -1,14 +1,9 @@
-using System.Runtime.Serialization;
-
 namespace Fambda.DataTypes
 {
-    [Serializable]
     public class AnotherException : Exception
     {
         public AnotherException() : base() { }
 
         public AnotherException(string message) : base(message) { }
-
-        protected AnotherException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- Fambda.Tests/DataTypes/SomeException.cs
+++ Fambda.Tests/DataTypes/SomeException.cs
@@ -1,14 +1,9 @@
-using System.Runtime.Serialization;
-
 namespace Fambda.DataTypes
 {
-    [Serializable]
     public class SomeException : Exception
     {
         public SomeException() : base() { }
 
         public SomeException(string message) : base(message) { }
-
-        protected SomeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
